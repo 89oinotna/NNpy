8f55358a14ce68454f283d93e1c1661d77f70b75
89oinotna <a.z.zegarelli@live.it>
2021-12-19 21:28:51 +0100

    learning rate decay

--- NNpy/optimizers.py
+++ NNpy/optimizers.py
@@ -26,7 +26,9 @@ minimum (as the exact gradient can do)
     """
 
     def __init__(self, ETA=0.01, weight_regularizer: reg.WeightRegularizer = None, ALPHA: float = 0.5,
-                 nesterov: bool = False, momentum_window: int = None):
+                 nesterov: bool = False, momentum_window: int = None, variable_eta: dict = None):
+        variable_eta['eta'] = ETA  # eta 0
+        self.variable_eta = variable_eta
         self.ETA = ETA
         self.nesterov = nesterov
         self.ALPHA = ALPHA
@@ -63,7 +65,7 @@ minimum (as the exact gradient can do)
             # only if the list has all the values
             if not (len(layer.list_old_delta_w) < self.momentum_window):
                 layer.old_delta_w -= layer.list_old_delta_w[0]
-                layer.list_old_delta_w=layer.list_old_delta_w[1:]
+                layer.list_old_delta_w = layer.list_old_delta_w[1:]
                 layer.list_old_delta_w.append(layer.delta_w)
             else:
                 layer.list_old_delta_w.append(layer.delta_w)
@@ -71,3 +73,17 @@ minimum (as the exact gradient can do)
 
         else:
             layer.old_delta_w = layer.delta_w
+
+        if self.variable_eta is not None:
+            self.decay_learning_rate()
+
+    def decay_learning_rate(self):
+        """
+        Using mini-batch → the gradient does not decrease to zero close to a
+        minimum (as the exact gradient can do)
+        • Hence fixed learning rate should be avoided:
+        """
+        if self.variable_eta['step'] < self.variable_eta['tau']:
+            self.variable_eta['step'] += 1
+            alpha = self.variable_eta['step'] / self.variable_eta['tau']
+            self.ETA = (1 - alpha) * self.variable_eta['eta'] + alpha * self.variable_eta['eta_tau']
