1f0ab19a4344f97781045a3f89ec085d3cc2fafa
Roberto <r.esposito130@studenti.unisa.it>
2022-01-03 12:39:59 +0100

    Grid Results

--- NNpy/__init__.py
+++ NNpy/__init__.py
@@ -1,34 +1,35 @@
 import numpy as np
 import network as nn
-import pandas as pd
-# print(wi.xavier_init(2,2)[0:, 1:])
 import activation_functions as af
 import losses
 import metrics
-from input_reading import read_monk, read_cup
+from input_reading import read_cup, read_monk
 import visualization as vis
 import grid_search as gs
-import itertools
-import logging
-from sklearn.model_selection import train_test_split
 
 #logging.basicConfig(level=logging.DEBUG)
-monk_data_tr, monk_tr_label, monk_vl_data, monk_vl_label, monk_test_data, monk_test_label= read_monk('monks-1')
-#train_data, valid_data, train_labels, valid_labels = train_test_split(
-#    train_data, train_label, test_size=0.2)
+
+#monk_data, monk_label = read_monk('monks-1.test')
+#monk_data_tr, monk_data_vl = np.array_split(monk_data,2)
+#monk_label_tr, monk_label_vl = np.array_split(monk_label,2)
+#print(monk_data[:1000, :])
 
 optimizer = {
     'type_init': 'sgd',
     'nesterov':True,
-    'ETA':0.5,
+    'ETA':0.8,
     #'momentum_window': 1,
-    'ALPHA':0.8
+    'ALPHA':0.8,
+    #'weight_regularizer': {
+    #    'type_init': 'tikonov',
+    #    'LAMBDA': 0.000
+    #}
 }
 wreg ={'type_init': 'tikonov', 'LAMBDA': 0.001}
 
-
+"""
 params = {
-    'layer_sizes': [(10, 70, 30, 2)],
+    'layer_sizes': [(35, 35, 2), (37, 37, 2), (41, 41, 2)],
     'act_hidden': ['sigmoid'],
     'act_out': ["id"],
     'w_init': ["xavier"],
@@ -38,66 +39,49 @@ params = {
     'optimizer': {
         'type_init': ['sgd'],
         #'nesterov':[True],
-        'ETA':np.random.uniform(0.055,0.08,8),#np.linspace(0.055, 0.076, 5),#[0.001, 0.01, 0.1, 0.2],
+        'ETA':[0.1, 0.06, 0.07, 0.08],
         #'momentum_window': 1,
-        'ALPHA': np.random.uniform(0.6,0.85,8),#np.linspace(0.60, 0.85, 5),
+        'ALPHA': [0.0, 0.2, 0.4, 0.6, 0.7, 0.8, 0.9],
         'weight_regularizer': {'type_init':['tikonov'],
-                              'LAMBDA':np.random.uniform(0, 7.5e-05, 8)#np.linspace(0, 7.5e-05, 6)
+                              'LAMBDA':[0.001, 0.0001, 0.00001]
                               },
     },
-    'batch_size': [32],
-    'epochs': [500]
+    'batch_size': [16, 32, 64],
+    'epochs': [500, 1000]
 }
 
-minibatch_monk = {'layer_sizes': (8, 2),
+minibatch = {'layer_sizes': (8, 2),
              'act_hidden': 'sigmoid',
              'act_out': 'id',
              'w_init': 'monk',
              'loss': 'mse',
              'metric': 'mee',
-             'optimizer': {'type_init': 'sgd', 'ETA': 0.3, 'ALPHA': 0.8,
+             'optimizer': {'type_init': 'sgd', 'ETA': 0.2, 'ALPHA': 0.8,
                            'weight_regularizer': {'type_init': 'tikonov',
-                                                  'LAMBDA': 0.0001
+                                                  'LAMBDA': 0.1
                                                   },
                            },
              'batch_size': 32, 'epochs': 500, 'input_size': 10}
-
-cup = {'layer_sizes': (10, 70, 30, 2),
-       'act_hidden': 'sigmoid',
-       'act_out': 'id',
-       'w_init': 'xavier',
-       'loss': 'mse',
-       'metric': 'mee',
-       'optimizer': {
-           'type_init': 'sgd',
-           'ETA': 0.1,
-           'ALPHA': 0.2,
-           'weight_regularizer': {
-               'type_init': 'tikonov',
-               'LAMBDA': 1e-05}
-       },
-       'batch_size': 32,
-       'epochs': 500,
-       'input_size': 10}
-
+"""
+# train_data, train_labels = read_monk("monks-1.train")
+# test_data, test_label = read_monk("monks-1.test")
 
 # train the best model
-#train_data, valid_data, train_labels, valid_labels = train_test_split(
+# train_data, valid_data, train_labels, valid_labels = train_test_split(
 #    train_data, train_label, test_size=0.2)
 
 if __name__ == '__main__':
-    train_data, train_labels, valid_data, valid_labels = read_cup(frac_train=0.8)  # read_monk("monks-1.train")
+    # train_data, train_labels, valid_data, valid_labels = read_cup(frac_train=0.8)  # read_monk("monks-1.train")
+    # minibatch = gs.grid_search_cv(params, train_data.values, train_labels)[0]
+    # network = nn.NeuralNetwork.init(**minibatch)
 
-    grid_results = gs.grid_search_cv(params, train_data.values, train_labels)
+    # (tr_metric, tr_loss), (vl_metric, vl_loss) = network.fit(train_data, train_labels, valid_data, valid_labels)
+    # vis.plot(tr_loss, vl_loss, tr_metric, vl_metric)
 
-    #grid_results = pd.read_csv("datasets/gs_results/grid_antonio_2.csv", sep=",")
-    network = nn.NeuralNetwork.init(**eval(grid_results.loc[0, 'network_topology']))
-
-    (tr_metric, tr_loss), (vl_metric, vl_loss) = network.fit(train_data, train_labels, valid_data, valid_labels, early_stopping=True)
+    train_data, train_labels, test_data, test_label = read_monk("monks-3")
+    network = nn.NeuralNetwork.init([15, 1], 17, act_hidden='relu', act_out='sigmoid', w_init='random_ranged', loss='mse', metric='simple_class', optimizer=optimizer, epochs=500)
+    (tr_metric, tr_loss), (vl_metric, vl_loss) = network.fit(train_data, train_labels, test_data, test_label)
     vis.plot(tr_loss, vl_loss, tr_metric, vl_metric)
-    #grid_results = pd.read_csv("./grid_antonio_1.csv", sep=",")
-    #print(grid_results.head())
-
 """
 network = nn.NeuralNetwork.init([4, 1], 17, act_hidden='relu', act_out='sigmoid', w_init='monk', loss='mse',
                                 metric='simple_class', optimizer=optimizer, weight_regularizer=wreg, epochs=400)
