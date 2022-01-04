84345b1085f5f60a1de52afe128de1a33b74d735
89oinotna <a.z.zegarelli@live.it>
2022-01-04 13:47:21 +0100

    fix

--- NNpy/network.py
+++ NNpy/network.py
@@ -149,9 +149,9 @@ class NeuralNetwork:
         vl_metric = []
         monitoring = None
         tr = None
-        labels_size=tr_label.shape[1]
-        tr_outputs = [] # predicted
-        vl_outputs = [] # predicted
+        labels_size = tr_label.shape[1]
+        tr_outputs = []  # predicted
+        vl_outputs = []  # predicted
         if self.minibatch_size is not None:  # to redo the splitting
             tr = pd.DataFrame(np.concatenate((tr_data, tr_label), axis=1))
 
@@ -183,8 +183,9 @@ class NeuralNetwork:
 
                 for j in range(int(np.ceil(tr.shape[0] / self.minibatch_size))):
                     # if last minibatch is smaller than minibatch_size
-                    batch_data = tr_data.iloc[j * self.minibatch_size:(j + 1) * self.minibatch_size, :] #- 1, :]
-                    batch_label = tr_label.iloc[(j * self.minibatch_size):((j + 1) * self.minibatch_size),:]# - 1), :]
+                    batch_data = tr_data.iloc[j * self.minibatch_size:(j + 1) * self.minibatch_size, :]  # - 1, :]
+                    batch_label = tr_label.iloc[(j * self.minibatch_size):((j + 1) * self.minibatch_size),
+                                  :]  # - 1), :]
                     self.step(batch_data, batch_label)
                 # todo add the possibility to use the mean over last n batches
                 output = self.feed_forward(tr_data)
@@ -226,15 +227,19 @@ class NeuralNetwork:
                         logging.debug("The epoch ", i, " improved the model ", monitor)
 
         if vl_data is not None:
-            return (tr_metric, tr_loss), (vl_metric, vl_loss), (tr_outputs, vl_outputs) if return_outputs \
-                else (tr_metric, tr_loss), (vl_metric, vl_loss)
-        return tr_metric, tr_loss, tr_outputs if return_outputs else tr_metric, tr_loss
+            if return_outputs:
+                return (tr_metric, tr_loss), (vl_metric, vl_loss), (tr_outputs, vl_outputs)
+            else:
+                return (tr_metric, tr_loss), (vl_metric, vl_loss)
+        if return_outputs:
+            return tr_metric, tr_loss, tr_outputs
+        else:
+            return tr_metric, tr_loss
 
     def save(self, path='./', name=None):
         if name is None:
-            name=str(hash(self))
+            name = str(hash(self))
         pickle.dump(self, open(f'{path}{name}.pkl', 'wb'))
-
 
     @staticmethod
     def load(path='./', name=None):
